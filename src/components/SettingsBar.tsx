e3e59df36c9c24207d44c99d5f37139f0ed12d1a
Trevor <trevorphilbrick@outlook.com>
2025-03-01 21:15:46 -0800

    moved signout button to bottom of settings bar

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -18,35 +18,39 @@ function SettingsBar() {
   };
 
   return (
-    <div className="w-96 h-full bg-neutral-900 text-neutral-300 border-l border-neutral-700">
-      <h1 className="text-2xl font-bold text-neutral-200">Settings</h1>
-      <RoundButton
-        icon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6 text-neutral-200"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-            />
-          </svg>
-        }
-        onClick={() => navigate("/")}
-        text="Back"
-      />
-      <p className="text-neutral-200">{user?.email}</p>
-      <button
-        className="bg-neutral-200 text-neutral-800 px-4 py-2 rounded-md"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>{" "}
+    <div className="w-96 h-full flex flex-col bg-neutral-900 text-neutral-300 border-r border-neutral-700 p-4 relevant">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-neutral-200">Settings</h1>
+        <RoundButton
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-neutral-200"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          }
+          onClick={() => navigate("/")}
+          text="Back"
+        />
+      </div>
+      <div className="absolute bottom-0 left-0 pb-4 px-4 flex flex-col gap-4 w-96">
+        <p className="text-neutral-200">{user?.email}</p>
+        <button
+          className="bg-neutral-200 text-neutral-800 px-4 py-2 rounded-md"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
